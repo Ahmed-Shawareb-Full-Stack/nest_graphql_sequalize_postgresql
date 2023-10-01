00c59889f7563f0b1467fcc6fea36135a582cd68
Ahmed-Shawareb-Full-Stack <js.dev.ahmedshawareb@gmail.com>
2023-10-01 22:39:20 +0300

    project : starting + installing Deps

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: +configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE'),
+        models: [],
+      }),
+      inject: [ConfigService],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), '../schema.gql'),
+      sortSchema: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
